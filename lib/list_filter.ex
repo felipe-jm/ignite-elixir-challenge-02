0e7fb0c5e897bf2215b37b810b38b6892722f2b6
Felipe Jung <felipemattoseu@gmail.com>
2021-03-08 12:29:43 -0400

    fix: make functions private

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -3,21 +3,21 @@ defmodule ListFilter do
 
   def call(list), do: even_numbers_count(list)
 
-  def even_numbers_count(list) do
+  defp even_numbers_count(list) do
     numbers = get_numbers(list)
     even_numbers = Enum.filter(numbers, fn number -> Integer.is_odd(number) end)
     length(even_numbers)
   end
 
-  def get_numbers(list) do
+  defp get_numbers(list) do
     numbers = Enum.filter(list, fn elem -> is_a_number(Integer.parse(elem)) end)
     string_list_to_integer(numbers)
   end
 
-  def is_a_number({number, _binary}), do: number
-  def is_a_number(:error), do: false
+  defp is_a_number({number, _binary}), do: number
+  defp is_a_number(:error), do: false
 
-  def string_list_to_integer(list) do
+  defp string_list_to_integer(list) do
     Enum.map(list, fn elem ->
         {number, _binary} = Integer.parse(elem)
         number
